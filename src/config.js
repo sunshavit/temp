a1d1c66dd57b6cc271ca9d613a0f22288127c62c
sunshavit <shavitsun7@gmail.com>
2021-02-14 18:01:09 +0200

    finished

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export const ACCUWEATHER_URL = "https://dataservice.accuweather.com";
 export const ACCUWEATHER_KEY = process.env.REACT_APP_API_WEATHER_KEY3;
+console.log(process.env);
 export const UNSPLASH_URL = "https://api.unsplash.com";
 export const SET_BACKGROUND_IMG = "process.env.REACT_APP_API_PHOTOS_KEY";
 export const UNSPLASH_KEY = process.env.REACT_APP_API_PHOTOS_KEY;
